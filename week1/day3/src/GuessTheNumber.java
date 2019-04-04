a89a137d65dde664ecadcddb6967e8fc5fcb69ed
Andras Knuth <knuth.andras89@gmail.com>
2019-04-04 17:23:16 +0200

    End of the day, Guess + drawDiagonal

--- week1/day3/src/GuessTheNumber.java
+++ week1/day3/src/GuessTheNumber.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
 
 public class GuessTheNumber {
-    public static void main(String[] args){
-        int n = 5;
-        System.out.println("Please guess a number!");
-        System.out.println("Now type the number please!");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        if (n > a){
-            System.out.println("The stored number is higher");
-        }else if (n < a){
-            System.out.println("The stored number is lower");
-        }else {
-            System.out.println("The number is" + a + "!");
+    public static void main(String[] args) {
+    int guess;
+    int number = 5;
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("I am thinking of a number, what do you think it is?");
+        guess = keyboard.nextInt();
+        while (guess != number) {
+        if (guess > number) {
+            System.out.println("Lower!");
+        }
+        else {
+            System.out.println("Higher!");
         }
     }
+        System.out.println(number + "is the correct answer");
+}
 }
 // Write a program that stores a number, and the user has to figure it out.
 // The user can input guesses, after each guess the program would tell one
