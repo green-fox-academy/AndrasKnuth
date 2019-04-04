cfaf3c66809adfcacdf30031e7641220d04991ea
Andras Knuth <knuth.andras89@gmail.com>
2019-04-04 16:17:54 +0200

    More exercises.

--- week1/day3/src/CountFromTo.java
+++ week1/day3/src/CountFromTo.java
@@ -1,22 +1,19 @@
+import java.util.Scanner;
+
 public class CountFromTo {
     public static void main(String[] args){
-
+        System.out.println("Number 1. please!");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("Number 2. please");
+        scanner = new Scanner(System.in);
+        int b = scanner.nextInt();
+        if ( a >= b ){
+            System.out.println("The second number should be bigger!");
+        }else if (a < b){
+            for (int i=a; i<b-1; i++ ){
+                System.out.println(a+i);
+            }
+        }
     }
-
 }
-
-
-
-// Create a program that asks for two numbers
-// If the second number is not bigger than the first one it should print:
-// "The second number should be bigger"
-//
-// If it is bigger it should count from the first number to the second by one
-//
-// example:
-//
-// first number: 3, second number: 6, should print:
-//
-// 3
-// 4
-// 5
---
+++ week1/day3/src/DrawTriangle.java
@@ -0,0 +1,22 @@
+public class DrawTriangle {
+    public static void main(String[] args){
+        
+
+
+
+
+    }
+
+}
+
+
+
+// Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
