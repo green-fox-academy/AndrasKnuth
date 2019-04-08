9ed456bc59425fa4d36f6d90fad232c2c549fadb
Andras Knuth <knuth.andras89@gmail.com>
2019-04-08 12:07:34 +0200

    Started the functions

---
+++ week2/day1/src/GreeterFunction.java
@@ -0,0 +1,21 @@
+public class GreeterFunction {
+    public static void main(String[] args) {
+        String name = ("al");
+        System.out.println(greet(name));
+
+    }
+
+    public static String greet(String in) {
+        return ("Greetings, " + in);
+    }
+}
+
+
+
+//  From now on, create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Greenfox`
+// - Greet `al`
