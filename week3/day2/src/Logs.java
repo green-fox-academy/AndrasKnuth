1436c834f03967a49f37c4f27058561dcf0c3c65
Andras Knuth <knuth.andras89@gmail.com>
2019-04-16 17:24:45 +0200

    Did not manage to do the Logs exercise.

--- week3/day2/src/Logs.java
+++ week3/day2/src/Logs.java
@@ -1,12 +1,10 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-
-
 
 public class Logs {
     public static void main(String[] args) {
@@ -15,28 +13,33 @@ public class Logs {
     }
 
     private static List IP(String file) {
-        List<String> list = new ArrayList<>();
         try {
-            Path filePath = Paths.get(file);
-            List<String> content = Files.readAllLines(filePath);
+
+            Path logFilePath = Paths.get(file);
+            List<String> content = Files.readAllLines(logFilePath);
+            List<String> IPAddresses = new ArrayList<>();
+            List<String> uniqueIPAddresses = new ArrayList<>();
+
             for (String line : content) {
-                String[] parts = line.split(" ");
-                if (!list.contains(parts[8])) {
-                    list.add(parts[8].trim());
+                IPAddresses.add(line.substring(27, 37));
+            }
+            for (String line : IPAddresses) {
+                if (uniqueIPAddresses.contains(IPAddresses)) {
+                } else {
+                    uniqueIPAddresses.add(line);
                 }
             }
+            return uniqueIPAddresses;
+
         } catch (IOException e) {
-            System.out.println("Could not open file: log.txt");
+            List<String> failedList = new ArrayList<>();
+            failedList.add("Failed to return IP addresses.");
+            return failedList;
         }
-        return list;
     }
 }
 
-
-
-
-
 // Read all data from 'log.txt'.
 // Each line represents a log message from a web server
-// Write a function that returns an array with the unique IP adresses.
+// Write a function that returns an array with the unique IP addresses.
 // Write a function that returns the GET / POST request ratio.
---
+++ week3/day2/src/TicTacToe.java
@@ -0,0 +1,19 @@
+/* public class TicTacToe {
+
+    public static void main(String[] args) {
+        // Write a function that takes a filename as a parameter
+        // The file contains an ended Tic-Tac-Toe match
+        // We have provided you some example files (draw.txt, win-x.txt, win-o.txt)
+        // Return "X", "O" or "Draw" based on the input file
+
+        System.out.println(ticTacResult("win-o.txt"))
+        // Should print "O"
+
+        System.out.println(ticTacResult("win-x.txt"))
+        // Should print "X"
+
+        System.out.println(ticTacResult("draw.txt"))
+        // Should print "Draw"
+    }
+}
+*/
