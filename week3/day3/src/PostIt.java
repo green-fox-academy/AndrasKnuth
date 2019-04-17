2d702176135d39b5788ee4803fcf31bfe2218fd1
Andras Knuth <knuth.andras89@gmail.com>
2019-04-17 11:23:35 +0200

    Started the day with PostIt

---
+++ week3/day3/src/PostIt.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+
+public class PostIt {
+    String BackgroundColor = new Color(Color);
+    String 
+
+}
+/*Create a PostIt class that has
+a backgroundColor
+a text on it
+a textColor
+Create a few example post-it objects:
+an orange with blue text: "Idea 1"
+a pink with black text: "Awesome"
+a yellow with green text: "Superb!"*/
