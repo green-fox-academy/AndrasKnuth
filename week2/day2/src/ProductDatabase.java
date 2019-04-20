6cd1235125c4f238dfd21100c3946eebb895ade4
Andras Knuth <knuth.andras89@gmail.com>
2019-04-20 21:37:53 +0200

    Completed the Product Database exercise.

--- week2/day2/src/ProductDatabase.java
+++ week2/day2/src/ProductDatabase.java
@@ -1,8 +1,6 @@
-import java.util.Collection;
-import java.util.Collections;
+import java.security.Key;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.IntStream;
 
 public class ProductDatabase {
     public static void main(String[] args) {
@@ -18,17 +16,17 @@ public class ProductDatabase {
         for (Map.Entry<String, Integer> entry : ProductDatabase.entrySet()) {
             sum += entry.getValue();
         }
-        System.out.println("The average of prices is: " + sum/ProductDatabase.size());
-
-
-        for (int x : ProductDatabase.values()) {
-            if (ProductDatabase.values() < 300){
-
+        System.out.println("The average of prices is: " + sum / ProductDatabase.size());
+        int i = 0;
+        for (String key : ProductDatabase.keySet()) {
+            if (ProductDatabase.get(key) < 300) {
+                i++;
             }
         }
-
         System.out.println(ProductDatabase.containsValue(125));
         System.out.println(ProductDatabase.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey());
         System.out.println(ProductDatabase.entrySet().stream().min((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey());
+        System.out.println("There are " + i + " items under 300.");
     }
 }
+
---
+++ week2/day2/src/ProductDatabase2.java
@@ -0,0 +1,2 @@
+public class ProductDatabase2 {
+}
