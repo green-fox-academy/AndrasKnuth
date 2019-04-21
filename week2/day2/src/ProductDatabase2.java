d6fbd7cc157b7e25b56ad9383ed3877af96630ad
Andras Knuth <knuth.andras89@gmail.com>
2019-04-21 10:44:34 +0200

    Practicing Lists and Hashmaps, ProductDatabse2 is done.

--- week2/day2/src/ProductDatabase2.java
+++ week2/day2/src/ProductDatabase2.java
@@ -1,2 +1,30 @@
+
+import java.util.HashMap;
+
 public class ProductDatabase2 {
+    
+    public static void main(String[] args) {
+        HashMap<String, Integer> ProductDatabase2 = new HashMap<>();
+        ProductDatabase2.put("Eggs", 200);
+        ProductDatabase2.put("Milk", 200);
+        ProductDatabase2.put("Fish", 400);
+        ProductDatabase2.put("Apples", 150);
+        ProductDatabase2.put("Bread", 50);
+        ProductDatabase2.put("Chicken", 550);
+        for (String i : ProductDatabase2.keySet()) {
+            if (ProductDatabase2.get(i).intValue() < 201) {
+                System.out.println(i);
+            }
+        }
+        for (String i : ProductDatabase2.keySet()) {
+            if (ProductDatabase2.get(i).intValue() > 150) {
+                System.out.println(i + " " + ProductDatabase2.get(i));
+            }
+        }
+    }
 }
+/*
+Create an application which solves the following problems.
+Which products cost less than 201? (just the name)
+Which products cost more than 150? (name + price)
+ */
