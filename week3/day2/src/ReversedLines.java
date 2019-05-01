e1b984666ab95037ba9a1de94c3192cf09ec3155
Andras Knuth <knuth.andras89@gmail.com>
2019-05-01 17:47:10 +0200

    feat(ReversedLines): do ReversedLines exercise

--- week3/day2/src/ReversedLines.java
+++ week3/day2/src/ReversedLines.java
@@ -1,2 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReversedLines {
+    public static void main(String[] args) {
+        String path = "src/reversed-lines.txt";
+        Path src = Paths.get(path);
+
+        try {
+            List<String> content = Files.readAllLines(src);
+            List <String> reversedContent = new ArrayList<>();
+            String reversedLine;
+
+            for (String line : content){
+                reversedLine = "";
+                char[] lineChars = line.toCharArray();
+                for(int i = 0; i <lineChars.length; i++){
+                    reversedLine += lineChars[lineChars.length - 1 - i];
+                }
+                reversedContent.add(reversedLine);
+
+            }
+            Files.write(src, reversedContent);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
+
