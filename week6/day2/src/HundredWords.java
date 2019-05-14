b62e4506f4a9cdbe4e34ddd521f7f2e10bde844a
Andras Knuth <knuth.andras89@gmail.com>
2019-05-14 17:36:54 +0200

    feat(HundredWords): method works, go through it!

--- week6/day2/src/HundredWords.java
+++ week6/day2/src/HundredWords.java
@@ -2,26 +2,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class HundredWords {
   public static void main(String[] args) {
-    String file = "src/wikipage.txt";
-    Path path = Paths.get(file);
+    List<String> content = readTheFile();
+    Map<String, Integer> wordOccupancies = content.stream()
+        .flatMap(str -> Arrays.stream(str.split("\\s")))
+        .map(gg -> gg.replaceAll("[^a-zA-Z ]", ""))
+        .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(c -> 1)));
 
+    wordOccupancies.entrySet().stream()
+        .limit(100)
+        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+        .filter(map -> !"".equals(map.getKey()))
+        .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+
+  }
+
+  private static List<String> readTheFile() {
+
+    List<String> content = new ArrayList<>();
+    Path filePath = Paths.get("src/wikipage.txt");
     try {
-      List<String> words = Files.readAllLines(path);
-      Map<String, Long> out = words.stream()
-          .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
-      System.out.println(out);
-    } catch (IOException e){
-      System.out.println("No file found goddammit!");
+      content = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      System.out.println("Oops, something wrong happened with the file!");
     }
+    return content;
   }
 }
+
+
 /*
 Create a Stream expression which reads all text from this file,
 and prints the 100 most common words in descending order.
