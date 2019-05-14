0fd6b20e5e3abe3f76728bd447d148c49dffc5c4
Andras Knuth <knuth.andras89@gmail.com>
2019-05-14 11:40:06 +0200

    feat(Exercise3): do Exercise3

--- week6/day2/src/Exercise3.java
+++ week6/day2/src/Exercise3.java
@@ -1,2 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise3 {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(3, 9, 2, 8, 6, 5);
+    numbers.stream()
+        .map(n -> n * n)
+        .filter(n -> n > 20)
+        .forEach(System.out::println);
+  }
 }
+//Write a Stream Expression to find which number squared value is more then 20 from the following list:
