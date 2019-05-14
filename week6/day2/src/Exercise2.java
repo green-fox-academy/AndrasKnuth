4e85b151b27e519b9ad443f7f89550eee900de25
Andras Knuth <knuth.andras89@gmail.com>
2019-05-14 11:31:22 +0200

    feat(Exercise2): create Exercise2

---
+++ week6/day2/src/Exercise2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise2 {
+  public static void main(String[] args) {
+    List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+    numbers.stream()
+        .filter(n -> n > 0)
+        .forEach(n -> n * 2)
+        .forEach(System.out::println);
+  }
+}
