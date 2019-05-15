c8b0308b8d1244af62bf744570884b08de7c9b41
Andras Knuth <knuth.andras89@gmail.com>
2019-05-15 15:32:47 +0200

    update(controllers): update HelloRest

--- week6/day3.1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week6/day3.1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -5,21 +5,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
-  String name = "Andras";
+  AtomicLong counter = new AtomicLong();
   @RequestMapping( value = "/greeting")
   @ResponseBody
-  public Greeting greeting(@RequestParam String name){
-    return new Greeting(1, "Hello, " + name + "!");
+  public Greeting helloRest(@RequestParam String name){
+    return new Greeting(counter.incrementAndGet(), "Hello, " + name + "!");
   }
 }
-
-/*
-Add greeting method and use @RequestMapping annotation.
-Create and return a Greeting object when it is called.
-Recompile app (use the "Make Project (Ctrl-F9" at the top of your project if you've
-edited the existing project and are restarting)
-Open http://localhost:8080/greeting in your web browser
-Your output should look like: {"id":1,"content":"Hello, World!"}
- */
