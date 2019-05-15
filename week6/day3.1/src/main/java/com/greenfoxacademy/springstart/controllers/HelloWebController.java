ceed28389d0e8faa1191a9b00eafd929dd372dc7
Andras Knuth <knuth.andras89@gmail.com>
2019-05-15 15:31:04 +0200

    feat(templates): add greeting.html

---
+++ week6/day3.1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HelloWebController {
+  @RequestMapping("/web/greeting")
+  public String greeting(Model model) {
+    model.addAttribute("name", " name");
+    return "greeting";
+  }
+}
