db41fde40a7b8c7810f18e84aa3e56b393e6d216
Andras Knuth <knuth.andras89@gmail.com>
2019-05-22 16:08:34 +0200

    feat(gfa): make basic structure

---
+++ week7/day3/gfa/src/main/java/com/greenfox/gfa/gfa/GfaApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.gfa.gfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GfaApplication.class, args);
+  }
+
+}
---
+++ week7/day3/gfa/src/main/java/com/greenfox/gfa/gfa/model/Student.java
@@ -0,0 +1,8 @@
+package com.greenfox.gfa.gfa.model;
+
+public class Student {
+  private String name;
+
+  public Student (String name){
+  }
+}
