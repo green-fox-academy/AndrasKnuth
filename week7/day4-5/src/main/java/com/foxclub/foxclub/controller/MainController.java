0dbbe5106e671dbd801304f62cb9cce37aa8ac2e
Andras Knuth <knuth.andras89@gmail.com>
2019-05-23 10:41:51 +0200

    feat(foxclub): make basic structure and homepage

---
+++ week7/day4-5/src/main/java/com/foxclub/foxclub/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.foxclub.foxclub.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping("/index")
+  public String mainPage(){
+    return "index";
+  }
+
+}
