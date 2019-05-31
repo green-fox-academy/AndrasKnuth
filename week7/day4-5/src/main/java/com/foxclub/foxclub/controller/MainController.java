20907484a2da36ceb215f9287015b405b1e0b126
Andras Knuth <knuth.andras89@gmail.com>
2019-05-31 10:05:47 +0200

    fix(foxClub): fix the MainController

--- week7/day4-5/src/main/java/com/foxclub/foxclub/controller/MainController.java
+++ week7/day4-5/src/main/java/com/foxclub/foxclub/controller/MainController.java
@@ -49,7 +49,7 @@ public class MainController {
 
   @PostMapping("/login")
   public String loggedIn(Model model, @ModelAttribute Fox fox) {
-    /*if ((foxList.addFox(fox) && !fox.getName().isEmpty()) || foxList.checkFoxExists(fox.getName()) && !fox.getName().isEmpty()) {
+    if ((foxList.addFox(fox) && !fox.getName().isEmpty()) || foxList.checkFoxExists(fox.getName()) && !fox.getName().isEmpty()) {
     } else {
       model.addAttribute("activeFoxIndex", activeFoxIndex);
       return "redirect:/login";
@@ -58,7 +58,7 @@ public class MainController {
     logger.saveLoginAction();
     model.addAttribute("fox", fox);
     model.addAttribute("activeFoxIndex", activeFoxIndex);
-    return "redirect:/"; */
+    return "redirect:/";
   }
 
   @GetMapping("/nutritionstore")
