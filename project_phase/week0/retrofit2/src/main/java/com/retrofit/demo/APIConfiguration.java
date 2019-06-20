ff1fd5a5d60a541f499cfddddbd485d9ee182e07
Andras Knuth <knuth.andras89@gmail.com>
2019-06-20 10:09:34 +0200

    feat(week0): retrofit screwing-around

---
+++ project_phase/week0/retrofit2/src/main/java/com/retrofit/demo/APIConfiguration.java
@@ -0,0 +1,7 @@
+package com.retrofit.demo;
+
+public interface APIConfiguration {
+  static final String API_BASE_URL = "https://api.github.com/";
+  static final String API_VERSION_SPEC = "application/vnd.github.v3+json";
+  static final String JSON_CONTENT_TYPE = "application/json";
+}
