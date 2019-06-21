c4ed31d0d5468549e8dc1b5d0e19c515e811b32c
Andras Knuth <knuth.andras89@gmail.com>
2019-06-21 15:50:13 +0200

    fix(JWT-main): add Bean for BCryptPasswordEncoder

---
+++ project_phase/week0/authentication/JWT/src/main/java/com/authentication/project/ProjectApplication.java
@@ -0,0 +1,19 @@
+package com.authentication.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectApplication.class, args);
+  }
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
